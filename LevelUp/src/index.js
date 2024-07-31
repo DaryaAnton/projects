ac7dc7db31faa045b49a0ac32bc07d7e73118001
Дарья Антон <167423043+DaryaAnton@users.noreply.github.com>
2024-07-31 17:07:16 +0500

    Create index.js

---
+++ LevelUp/src/index.js
@@ -0,0 +1,9 @@
+import carousel from './modules/carousel';
+import scroll from './modules/scroll';
+import timer from './modules/timer';
+import validation from './modules/validation';
+
+carousel();
+scroll();
+timer();
+validation();
