c38d3ea5cd05460642179c38d568e24d9a505ee6
Дарья Антон <167423043+DaryaAnton@users.noreply.github.com>
2024-07-31 18:37:16 +0500

    Create index.js

---
+++ electrics/src/index.js
@@ -0,0 +1,15 @@
+import modal from './modules/modal';
+import slider from './modules/slider';
+import scroll from './modules/scroll';
+import carousel from './modules/carousel';
+import accordeon from './modules/accordeon';
+import form from './modules/form';
+import validation from './modules/validation';
+
+modal();
+slider();
+scroll();
+carousel();
+accordeon();
+form();
+validation();
